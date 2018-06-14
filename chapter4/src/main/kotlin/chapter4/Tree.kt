1a1bfb52374903afd8a2bfeb0112661bf3e5eb62
Amit Dawle <amit.dawle@gmail.com>
2018-06-14 22:22:31 +0100

    Updated tests

--- chapter4/src/main/kotlin/chapter4/Tree.kt
+++ chapter4/src/main/kotlin/chapter4/Tree.kt
@@ -1,5 +1,7 @@
 package chapter4
 
+import kotlin.math.E
+
 
 sealed class Tree<out T>
 data class Node<out T>(val value:T, val left: Tree<T>, val right: Tree<T>) : Tree<T>()
@@ -7,13 +9,15 @@ object Empty : Tree<Nothing>()
 
 
 fun  minimalTree(list : List<Int>) : Tree<Int> {
-       return when(list.isEmpty()) {
-           true -> Empty
+       return when(list.size) {
+           0 -> Empty
+           1 -> Node(list[0], Empty, Empty)
+           2 -> Node(list[1], Node(list[0], Empty, Empty), Empty)
            else -> {
-               val mid = list[list.size / 2]
-               Node(mid,
-                       minimalTree(list.filter { it < mid }),
-                       minimalTree(list.filter { it > mid }))
+               val mid = list.size / 2
+               Node(list[mid],
+                       minimalTree(list.subList(0, mid)),
+                       minimalTree(list.subList(mid + 1, list.size)))
            }
        }
 }
--- chapter4/src/test/kotlin/chapter4/TreeTest.kt
+++ chapter4/src/test/kotlin/chapter4/TreeTest.kt
@@ -10,24 +10,46 @@ class TreeTest : Spek({
 
 
     describe("Build minimal tree") {
-        given("An empty list") {
-            it("Should build an Empty tree") {
+        given("an empty list") {
+            it("should build an Empty tree") {
                 assertEquals(minimalTree(emptyList()), Empty)
             }
         }
-        given("A list with one element") {
-            it("Should build a tree with one Node") {
+        given("a list with one element") {
+            it("should build a tree with one Node") {
                 assertEquals(minimalTree(listOf(1)), Node(1, Empty, Empty))
             }
         }
-        given("A list with two elements") {
-            it("Should build a tree with 2 Nodes with second node at mid as parent") {
+        given("a list with two elements") {
+            it("should build a tree with 2 Nodes with second node at mid as parent") {
                 assertEquals(minimalTree(listOf(1,2)), Node(2, Node(1, Empty, Empty),Empty))
             }
         }
-        given("A list with three elements") {
-            it("Should build a tree with 3 Nodes with the node at mid as parent") {
+        given("a list with three elements") {
+            it("should build a tree with 3 Nodes with the node at mid as parent") {
                 assertEquals(minimalTree(listOf(1,2,3)), Node(2, Node(1, Empty, Empty), Node(3, Empty, Empty)))
+            }
+        }
+        given("a list with some odd elements") {
+            it("should build a tree with some Nodes with the node at mid as parent") {
+                assertEquals(minimalTree(listOf(1,2,3,4,5)),
+                                        Node(3,
+                                                 Node(2, Node(1, Empty, Empty), Empty),
+                                                 Node(5, Node(4, Empty, Empty), Empty)
+                                        )
+                        )
+            }
+        }
+        given("a list with some even elements") {
+            it("should build a tree with some Nodes with the node at mid as parent") {
+                assertEquals(minimalTree(listOf(1,2,3,4,5,6)),
+                        Node(4,
+                                Node(2, Node(1, Empty, Empty), Node(3, Empty, Empty)),
+                                Node(6,
+                                        Node(5, Empty, Empty),
+                                        Empty)
+                        )
+                )
             }
         }
    }
