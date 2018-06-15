bcc335c97d24f36337d31801e2f417ad12e36fc6
Amit Dawle <amit.dawle@ihsmarkit.com>
2018-06-15 14:52:26 +0100

    Fixed tests and updated conditions.

--- chapter4/src/main/kotlin/chapter4/Tree.kt
+++ chapter4/src/main/kotlin/chapter4/Tree.kt
@@ -4,17 +4,17 @@ sealed class Tree<out T>
 data class Node<out T>(val value:T, val left: Tree<T>, val right: Tree<T>) : Tree<T>()
 object Empty : Tree<Nothing>()
 
+fun  <T> minimalTree(list : List<T>) : Tree<T> {
+        return when(list.size){
+            0 -> Empty
+            else -> {
+                val mid = list.size / 2
+                return  Node(list[mid],
+                        minimalTree(list.take(mid)),
+                        minimalTree(list.drop(mid + 1)))
 
-fun  minimalTree(list : List<Int>) : Tree<Int> {
-       return when(list.size) {
-           0 -> Empty
-           1 -> Node(list[0], Empty, Empty)
-           2 -> Node(list[1], Node(list[0], Empty, Empty), Empty)
-           else -> {
-               val mid = list.size / 2
-               Node(list[mid],
-                       minimalTree(list.subList(0, mid)),
-                       minimalTree(list.subList(mid + 1, list.size)))
-           }
-       }
-}
+            }
+        }
+ }
+
+
--- chapter4/src/test/kotlin/chapter4/TreeTest.kt
+++ chapter4/src/test/kotlin/chapter4/TreeTest.kt
@@ -1,6 +1,6 @@
 package chapter4
 
-import junit.framework.Assert.assertEquals
+import org.junit.Assert.assertEquals
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
@@ -12,43 +12,42 @@ class TreeTest : Spek({
     describe("Build minimal tree") {
         given("an empty list") {
             it("should build an Empty tree") {
-                assertEquals(minimalTree(emptyList()), Empty)
+                assertEquals(Empty, minimalTree(emptyList<Int>()))
             }
         }
         given("a list with one element") {
             it("should build a tree with one Node") {
-                assertEquals(minimalTree(listOf(1)), Node(1, Empty, Empty))
+                assertEquals(Node(1, Empty, Empty), minimalTree(listOf(1)))
             }
         }
         given("a list with two elements") {
             it("should build a tree with 2 Nodes with second node at mid as parent") {
-                assertEquals(minimalTree(listOf(1,2)), Node(2, Node(1, Empty, Empty),Empty))
+                assertEquals(Node(2, Node(1, Empty, Empty),Empty), minimalTree(listOf(1,2)))
             }
         }
         given("a list with three elements") {
             it("should build a tree with 3 Nodes with the node at mid as parent") {
-                assertEquals(minimalTree(listOf(1,2,3)), Node(2, Node(1, Empty, Empty), Node(3, Empty, Empty)))
+                assertEquals(Node(2, Node(1, Empty, Empty), Node(3, Empty, Empty)), minimalTree(listOf(1,2,3)))
             }
         }
         given("a list with some odd elements") {
             it("should build a tree with some Nodes with the node at mid as parent") {
-                assertEquals(minimalTree(listOf(1,2,3,4,5)),
-                                        Node(3,
+                assertEquals(Node(3,
                                                  Node(2, Node(1, Empty, Empty), Empty),
                                                  Node(5, Node(4, Empty, Empty), Empty)
-                                        )
+                                        ), minimalTree(listOf(1,2,3,4,5))
                         )
             }
         }
         given("a list with some even elements") {
             it("should build a tree with some Nodes with the node at mid as parent") {
-                assertEquals(minimalTree(listOf(1,2,3,4,5,6)),
-                        Node(4,
+                assertEquals(Node(4,
                                 Node(2, Node(1, Empty, Empty), Node(3, Empty, Empty)),
                                 Node(6,
                                         Node(5, Empty, Empty),
                                         Empty)
-                        )
+                              ),
+                        minimalTree(listOf(1,2,3,4,5,6))
                 )
             }
         }
