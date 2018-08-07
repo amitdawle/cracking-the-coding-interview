6eff81d3c2f8f03c8fbff2989698970fb1030172
Amit Dawle <amit.dawle@ihsmarkit.com>
2018-08-07 09:38:17 +0100

    Updated tests

--- chapter2/src/main/kotlin/chapter2/Lists.kt
+++ chapter2/src/main/kotlin/chapter2/Lists.kt
@@ -75,23 +75,67 @@ private fun <T> Node<T>.drop(k: Int): Node<T> {
 }
 
 
-fun sumListReverseOrder(one: List<Int> , two: List<Int> ) : List<Int> {
+fun sumList(one: List<Int> , two: List<Int> ) : List<Int> {
 
-    fun sum(s: List<Int>, l: List<Int>, carry: Int ) : List<Int> {
-      return when(s.isEmpty()) {
-            true ->  if (carry == 0 ) l else if (carry != 0 && l.isEmpty()) listOf(carry) else sum(listOf(carry), l, 0)
+    val a = listToNumber(one)
+    val b = listToNumber(two)
+    val sum = a + b
+    return numberToList(sum)
+}
+
+fun numberToList(sum: Int): List<Int> {
+    if( sum < 10 ){
+        return listOf(sum)
+    }
+    fun toList(n: Int, soFar: MutableList<Int>) : List<Int> {
+        return when(n == 0) {
+            true -> soFar
             else -> {
-                val a = s[0]
-                val b = l[0]
-                val total = a + b + carry
-                listOf(total % 10) + sum(s.drop(1), l.drop(1), (total) / 10)
+                soFar.add(0, n % 10)
+                toList(n / 10 , soFar)
             }
-
         }
     }
-    val (l, s) = if( one.size > two.size) Pair(one, two) else Pair(two, one)
-    return sum(s, l, 0)
+    return toList(sum, mutableListOf())
+}
 
+
+fun sumListReverseOrder(one: List<Int> , two: List<Int> ) : List<Int> {
+
+    val a = reverseListToNumber(one)
+    val b = reverseListToNumber(two)
+    val sum = a + b
+    return  numberToReverseList(sum)
+}
+
+fun numberToReverseList(input: Int) : List<Int>{
+    if( input == 0){
+        return listOf(0)
+    }
+
+    fun toList(i: Int): List<Int>{
+         return when( i == 0) {
+             true -> return emptyList()
+             false -> listOf(i % 10) + toList(i / 10)
+         }
+    }
+    return toList(input)
+}
+
+
+
+fun reverseListToNumber(input: List<Int>) : Int{
+  return input.fold(Pair(0,1)){ (sum, pow), x -> Pair(x * pow + sum, pow * 10 )}.first
+}
+
+
+
+
+fun listToNumber(input: List<Int>) : Int{
+    return when(input.size == 1){
+        true -> input[0]
+        else ->  input.drop(1).fold(input[0]) {t ,x -> x + t * 10 }
+    }
 }
 
 fun <T> buildFrom(l : List<T>) : Node<T> {
@@ -101,4 +145,11 @@ fun <T> buildFrom(l : List<T>) : Node<T> {
 fun main(args: Array<String>) {
     println(sumListReverseOrder(listOf(1), listOf(9,9)))
     println(sumListReverseOrder(listOf(1,1), listOf(8,9,9,9,9)))
+    println(sumListReverseOrder(listOf(0), listOf(0)))
+    println(sumListReverseOrder(listOf(-1), listOf(1)))
+
+
+    println(sumList(listOf(1), listOf(9,9)))
+    println(sumList(listOf(1, 5), listOf(9,9)))
+    println(sumList(listOf(1, 2, 1), listOf(8,9)))
 }
